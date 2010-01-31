e06add64467867fe2b8a25a9da96a83b3faecfa5
bll <none@none>
2010-01-31 12:20:04 -0800

    build in tmp dir; separate hdrs

--- tests.d/c.d/c-multlib.sh
+++ tests.d/c.d/c-multlib.sh
@@ -5,13 +5,19 @@ echo ${EN} "lib w/multiple req${EC}" >&3
 
 grc=0
 
-export CFLAGS="-I. -I.. ${CFLAGS}"
-export LDFLAGS="-L.. ${LDFLAGS}"
+TMP=_tmp_test_03
+test -d $TMP && rm -rf $TMP
+mkdir $TMP
 
-rm -f libtest03?.* test03?.* > /dev/null 2>&1
+export CFLAGS="-I../$TMP ${CFLAGS}"
+export LDFLAGS="-L../$TMP ${LDFLAGS}"
+
+cd $TMP
 
 cat > test03b.h <<_HERE_
 int test03b ();
+_HERE_
+cat > test03c.h <<_HERE_
 int test03c ();
 _HERE_
 
@@ -19,21 +25,36 @@ cat > test03b.c <<_HERE_
 #include <stdio.h>
 #include <stdlib.h>
 #include <test03b.h>
+#include <test03c.h>
 int test03b () { test03c(); }
 _HERE_
 ${CC} -c ${CFLAGS} test03b.c
+if [ $? -ne 0 ]; then 
+  echo "compile test03b.c failed"
+  cd ..
+  test -d $TMP && rm -rf $TMP
+  exit 1
+fi
 ar cq libtest03b.a test03b.o
 
 cat > test03c.c <<_HERE_
 #include <stdio.h>
 #include <stdlib.h>
-#include <test03b.h>
+#include <test03c.h>
 int test03c () { printf ("hello world\n"); }
 _HERE_
 ${CC} -c ${CFLAGS} test03c.c
+if [ $? -ne 0 ]; then 
+  echo "compile test03b.c failed"
+  cd ..
+  test -d $TMP && rm -rf $TMP
+  exit 1
+fi
 ar cq libtest03c.a test03c.o
 
-../${script} test_03.dat
+cd ..
+
+../${script} -C test_03.dat
 diff -b test_03.configh config.h
 rc=$?
 if [ $rc -ne 0 ];then grc=$rc; fi
@@ -42,7 +63,7 @@ diff -b test_03.reqlibs reqlibs.txt
 rc=$?
 if [ $rc -ne 0 ];then grc=$rc; fi
 
-rm -f libtest03?.* test03?.* > /dev/null 2>&1
+test -d $TMP && rm -rf $TMP 
 
 echo "## config.h"
 cat config.h
