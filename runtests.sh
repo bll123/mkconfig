94df83f1b12669fe79f9e0244b42e071ab8c1c58
bll <none@none>
2010-01-31 09:41:06 -0800

    script as arg; edit config diff file

--- runtests.sh
+++ runtests.sh
@@ -2,8 +2,9 @@
 
 export CC=cc
 
-EN='-n'
-EC=''
+export EN='-n'
+export EC=''
+exec 3>&1
 
 echo -n 'test' | grep -- '-n' > /dev/null 2>&1
 rc=$?
@@ -14,31 +15,73 @@ then
 fi
 
 clean () {
+  tbase=$1
   rm -rf mkconfig.log mkconfig.cache _tmp_mkconfig \
-    reqlibs.txt config.h > /dev/null 2>&1
+    reqlibs.txt config.h $tbase.configh > /dev/null 2>&1
 }
 
 count=0
 fcount=0
 for tf in test_??.sh
 do
-  tlog=`echo $tf | sed 's/\.sh$/.log/'`
-  echo ${EN} "$tf ... ${EC}"
-  clean
-  echo "## stdout" > ${tlog}
-  ./$tf >> ${tlog} 2>&1
-  rc=$?
-  if [ $rc -ne 0 ]
-  then
-    echo "failed"
+  tbase=`echo $tf | sed 's/\.sh$//'`
+  tlog="${tbase}.log"
+  tconfig="${tbase}.config"
+  tconfh="${tbase}.configh"
+
+  clean $tbase
+  if [ ! -x ./$tf ]; then
+    echo "permission denied"
     fcount=`expr $fcount + 1`
-    echo "## mkconfig.log" >> ${tlog}
-    cat mkconfig.log >> ${tlog}
-  else
-    echo "success"
-    rm -f ${tlog} 
+    count=`expr $count + 1`
+    rm -f ${tlog}
+    clean $tbase
+    continue
   fi
-  clean
+
+  grc=0
+  echo "## === mkconfig.sh " > ${tlog}
+  echo "## stdout" >> ${tlog}
+  echo ${EN} "$tf ... mkconfig.sh: ${EC}"
+  if [ -f $tconfig ]; then
+    cp -pf $tconfig $tconfh
+  fi
+  ./$tf mkconfig.sh >> ${tlog} 2>&1
+  rc=$?
+  echo "## mkconfig.log" >> ${tlog}
+  cat mkconfig.log >> ${tlog}
+  if [ $rc -ne 0 ]; then
+    echo " ... failed"
+    fcount=`expr $fcount + 1`
+    grc=1
+  else
+    echo " ... success"
+  fi
+  clean $tbase
+  count=`expr $count + 1`
+
+  echo ${EN} "$tf ... mkconfig.pl: ${EC}"
+  echo "## === mkconfig.pl " >> ${tlog}
+  echo "## stdout" >> ${tlog}
+  if [ -f $tconfig ]; then
+    cat $tconfig | sed 's/_mkconfig_sh 1/_mkconfig_sh 0/' |
+      sed 's/_mkconfig_pl 0/_mkconfig_pl 1/' > $tconfh
+  fi
+  ./$tf mkconfig.pl >> ${tlog} 2>&1
+  rc=$?
+  echo "## mkconfig.log" >> ${tlog}
+  cat mkconfig.log >> ${tlog}
+  if [ $rc -ne 0 ]; then
+    echo " ... failed"
+    fcount=`expr $fcount + 1`
+  else
+    echo " ... success"
+    if [ $grc -eq 0 ]; then
+      rm -f ${tlog}
+    fi
+  fi
+  clean $tbase
+
   count=`expr $count + 1`
 done
 
