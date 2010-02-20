51852cd20fc383273fa43177b33858d1aa0bb964
bll <none@none>
2010-02-19 21:34:38 -0800

    Initial revision

---
+++ tests.d/compmksh.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+script=$@
+echo ${EN} "compile mkconfig.sh${EC}" >&3
+
+grc=0
+
+/bin/sh -n ../mkconfig.sh
+rc=$?
+if [ $rc -ne 0 ];then grc=$rc; fi
+
+if [ -x /usr/bin/ksh ]; then
+  /usr/bin/ksh -n ../mkconfig.sh
+  rc=$?
+  if [ $rc -ne 0 ];then grc=$rc; fi
+fi
+
+if [ -x /bin/bash ]; then
+  /bin/bash -n ../mkconfig.sh
+  rc=$?
+  if [ $rc -ne 0 ];then grc=$rc; fi
+fi
+
+exit $grc
---
+++ tests.d/compsh.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+script=$@
+echo ${EN} "compile mkconfig units${EC}" >&3
+
+grc=0
+
+cd ../mkconfig.units
+for f in *.sh; do
+  /bin/sh -n $f
+  rc=$?
+  if [ $rc -ne 0 ];then grc=$rc; fi
+
+  if [ -x /usr/bin/ksh ]; then
+    /usr/bin/ksh -n $f
+    rc=$?
+    if [ $rc -ne 0 ];then grc=$rc; fi
+  fi
+
+  if [ -x /bin/bash ]; then
+    /bin/bash -n $f
+    rc=$?
+    if [ $rc -ne 0 ];then grc=$rc; fi
+  fi
+done
+
+exit $grc
