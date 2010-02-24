c723bf76cf90bff1cec5b1417d38cf86532e0e90
bll <none@none>
2010-02-24 15:07:48 -0800

    test missing 2>/dev/null

--- bin/shellfuncs.sh
+++ bin/shellfuncs.sh
@@ -17,7 +17,7 @@ testshcapability () {
   if [ $? -eq 0 ]; then
     shhasappend=1
   fi
-  ( eval "x=bcb;y=\${x/c/_};test z\$y = zb_b")
+  ( eval "x=bcb;y=\${x/c/_};test z\$y = zb_b") 2>/dev/null
   if [ $? -eq 0 ]; then
     shhasparamsub=1
     # old freebsd shell complains if this substitution is inline.
