8789a293f4f14a30ddd1576d44c4c1ed35d0ceab
bll <none@none>
2010-03-01 08:51:46 -0800

    handle proto/keys not supported

--- tests.d/c.d/c-include.sh
+++ tests.d/c.d/c-include.sh
@@ -5,6 +5,12 @@ script=$@
 echo ${EN} "include${EC}" >&3
 eval "${script} -C test_04.dat"
 echo "## diff"
+ed test_04.ctest << _HERE_ > /dev/null 2>&1
+g/^#define _key_/d
+g/^#define _proto_/d
+w
+q
+_HERE_
 diff -b test_04.ctmp test_04.ctest
 rc=$?
 echo "## config.h"
--- tests.d/c.d/c-multlib.sh
+++ tests.d/c.d/c-multlib.sh
@@ -57,6 +57,12 @@ cd ..
 
 eval "${script} -C test_06.dat"
 echo "## diff 1"
+ed test_06.ctest << _HERE_ > /dev/null 2>&1
+g/^#define _key_/d
+g/^#define _proto_/d
+w
+q
+_HERE_
 diff -b test_06.ctmp test_06.ctest
 rc=$?
 if [ $rc -ne 0 ];then grc=$rc; fi
