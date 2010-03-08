e31c0d20e18bebdd579d19e0eede7b2ee629c8ea
bll <none@none>
2010-03-07 22:14:03 -0800

    Initial revision

---
+++ util/mktar.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "Enter version: \c"
+read ver
+
+PKG=mkconfig
+dir="${PKG}-${ver}"
+rm -rf ${dir} > /dev/null 2>&1
+mkdir ${dir}
+chmod 755 ${dir}
+
+cat MANIFEST | sed 's,[/]*[	 ].*$,,' |
+while read f; do
+  if [ -d $f ]; then
+    mkdir ${dir}/${f}
+    chmod 755 ${dir}/${f}
+  else
+    d=$(dirname $f)
+    cp $f ${dir}/${d}
+  fi
+done
+chmod -R a+r ${dir}
+
+tar cf - ./${dir} |
+gzip -9 > ${dir}.tar.gz
+
+rm -rf ${dir} > /dev/null 2>&1
+
+exit 0
