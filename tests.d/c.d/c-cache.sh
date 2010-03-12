0d2397c14fd062bcd85c571a248a7c08845c489a
bll <none@none>
2010-03-11 23:54:59 -0800

    rename of vars file

--- tests.d/c.d/c-cache.sh
+++ tests.d/c.d/c-cache.sh
@@ -6,10 +6,10 @@ echo ${EN} "cache${EC}" >&3
 rm -f test_09.a test_09.b test_09.a.vars test_09.b.vars test_09.ctest
 eval "${script} -C test_09.dat"
 mv -f test_09.ctest test_09.a
-mv -f mkconfig.vars test_09.a.vars
+mv -f mkconfig_c.vars test_09.a.vars
 eval "${script} test_09.dat"
 mv -f test_09.ctest test_09.b
-mv -f mkconfig.vars test_09.b.vars
+mv -f mkconfig_c.vars test_09.b.vars
 grc=0
 echo "## diff config.h"
 diff -b test_09.a test_09.b
