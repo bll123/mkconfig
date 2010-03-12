3f030f651b55e0d37fa3e709cebb5c8b14ac3c4b
bll <none@none>
2010-03-11 23:55:37 -0800

    Initial revision

---
+++ units/env-main.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+#
+# Copyright 2010 Brad Lanam Walnut Creek CA USA
+#
+#
+
+_MKCONFIG_PREFIX=env
+_MKCONFIG_HASEMPTY=T
+
+preconfigfile () {
+  pc_configfile=$1
+  return
+}
+
+stdconfigfile () {
+  pc_configfile=$1
+  return
+}
+
+postconfigfile () {
+  pc_configfile=$1
+  return
+}
+
+standard_checks () {
+  return
+}
+
+output_item () {
+  out=$1
+  name=$2
+  val=$3
+
+  echo "${name}=\"${val}\"" >> ${out}
+  echo "export ${name}" >> ${out}
+}
+
+output_other () {
+  return
+}
+
+set_other_name () {
+  arg=$1
+  eval $arg
+}
