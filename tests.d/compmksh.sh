0d3238a50b1f670fb7508aceff7a48ecf7762185
bll <none@none>
2010-03-23 22:29:13 -0700

    echo shell type

--- tests.d/compmksh.sh
+++ tests.d/compmksh.sh
@@ -5,6 +5,7 @@ echo ${EN} "compile mkconfig.sh${EC}" >&3
 
 grc=0
 
+echo ${EN} " ${EC}" >&3
 for s in /bin/sh /bin/bash /bin/posh /bin/ash /bin/dash; do
   if [ -x $s ]; then
     ls -l $s | grep -- '->' > /dev/null 2>&1
@@ -12,6 +13,9 @@ for s in /bin/sh /bin/bash /bin/posh /bin/ash /bin/dash; do
     ls -l $s | grep '/etc/alternatives' > /dev/null 2>&1
     rc2=$?
     if [ $rc1 -ne 0 -o $rc2 -eq 0 ]; then
+      sh=`echo $s | sed 's,.*/,,'`
+      echo ${EN} "${sh} ${EC}" >&3
+      echo "   testing with ${sh} "
       $s -n $RUNTOPDIR/mkconfig.sh
       rc=$?
       if [ $rc -ne 0 ];then grc=$rc; fi
@@ -39,6 +43,9 @@ for s in /usr/bin/ksh /bin/ksh; do
         pdksh)
           ;;
         *)
+          sh=`echo $s | sed 's,.*/,,'`
+          echo ${EN} "${sh} ${EC}" >&3
+          echo "   testing with ${sh} "
           $s -n $RUNTOPDIR/mkconfig.sh
           rc=$?
           if [ $rc -ne 0 ];then grc=$rc; fi
--- tests.d/compsh.sh
+++ tests.d/compsh.sh
@@ -5,50 +5,59 @@ echo ${EN} "compile mkconfig units${EC}" >&3
 
 grc=0
 
+echo ${EN} " ${EC}" >&3
 cd $RUNTOPDIR/mkconfig.units
-for f in *.sh; do
-  for s in /bin/sh /bin/bash /bin/posh /bin/ash /bin/dash; do
-    if [ -x $s ]; then
-      ls -l $s | grep -- '->' > /dev/null 2>&1
-      rc1=$?
-      ls -l $s | grep '/etc/alternatives' > /dev/null 2>&1
-      rc2=$?
-      if [ $rc1 -ne 0 -o $rc2 -eq 0 ]; then
+for s in /bin/sh /bin/bash /bin/posh /bin/ash /bin/dash; do
+  if [ -x $s ]; then
+    ls -l $s | grep -- '->' > /dev/null 2>&1
+    rc1=$?
+    ls -l $s | grep '/etc/alternatives' > /dev/null 2>&1
+    rc2=$?
+    if [ $rc1 -ne 0 -o $rc2 -eq 0 ]; then
+      sh=`echo $s | sed 's,.*/,,'`
+      echo ${EN} "${sh} ${EC}" >&3
+      echo "   testing with ${sh} "
+      for f in *.sh; do
         $s -n $f
         rc=$?
         if [ $rc -ne 0 ];then grc=$rc; fi
-      fi
+      done
     fi
-  done
+  fi
+done
 
-  for s in /usr/bin/ksh /bin/ksh; do
-    if [ $s = "/bin/ksh" ]; then
-      ls -l /bin | grep -- '->' > /dev/null 2>&1
-      if [ $? -eq 0 ]; then
-        continue
-      fi
+for s in /usr/bin/ksh /bin/ksh; do
+  if [ $s = "/bin/ksh" ]; then
+    ls -l /bin | grep -- '->' > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+      continue
     fi
+  fi
 
-    if [ -x $s ]; then
-      ls -l $s | grep -- '->' > /dev/null 2>&1
-      rc1=$?
-      ls -l $s | grep '/etc/alternatives' > /dev/null 2>&1
-      rc2=$?
-      if [ $rc1 -ne 0 -o $rc2 -eq 0 ]; then
-        cmd="$s -c \". $mypath/shellfuncs.sh;getshelltype;echo \\\$shell\""
-        tshell=`eval $cmd`
-        case $tshell in
-          pdksh)
-            ;;
-          *)
+  if [ -x $s ]; then
+    ls -l $s | grep -- '->' > /dev/null 2>&1
+    rc1=$?
+    ls -l $s | grep '/etc/alternatives' > /dev/null 2>&1
+    rc2=$?
+    if [ $rc1 -ne 0 -o $rc2 -eq 0 ]; then
+      cmd="$s -c \". $mypath/shellfuncs.sh;getshelltype;echo \\\$shell\""
+      tshell=`eval $cmd`
+      case $tshell in
+        pdksh)
+          ;;
+        *)
+          sh=`echo $s | sed 's,.*/,,'`
+          echo ${EN} "${sh} ${EC}" >&3
+          echo "   testing with ${sh} "
+          for f in *.sh; do
             $s -n $f
             rc=$?
             if [ $rc -ne 0 ];then grc=$rc; fi
-            ;;
-        esac
-      fi
+          done
+          ;;
+      esac
     fi
-  done
+  fi
 done
 
 exit $grc
