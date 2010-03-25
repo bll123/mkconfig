fd0d66879143a1da616cc7cc193a47580f5d562a
bll <none@none>
2010-03-24 23:07:35 -0700

    use func; chg paths

--- tests.d/compmksh.sh
+++ tests.d/compmksh.sh
@@ -3,57 +3,19 @@
 script=$@
 echo ${EN} "compile mkconfig.sh${EC}" >&3
 
+. $MKCONFIG_DIR/shellfuncs.sh
 grc=0
 
 echo ${EN} " ${EC}" >&3
-
-plist=""
-ls -ld /bin | grep -- '->' > /dev/null 2>&1
-if [ $? -ne 0 ]; then
-  plist=/bin
-fi
-plist="${plist} /usr/bin /usr/local/bin"
-
-for p in $plist; do
-  for s in sh bash posh ash dash mksh; do
-    if [ -x $p/$s ]; then
-      ls -l $p/$s | grep -- '->' > /dev/null 2>&1
-      rc1=$?
-      ls -l $p/$s | grep '/etc/alternatives' > /dev/null 2>&1
-      rc2=$?
-      if [ $rc1 -ne 0 -o $rc2 -eq 0 ]; then
-        echo ${EN} "${s} ${EC}" >&3
-        echo "   testing with ${s} "
-        $p/$s -n $RUNTOPDIR/mkconfig.sh
-        rc=$?
-        if [ $rc -ne 0 ];then grc=$rc; fi
-      fi
-    fi
-  done
-
-  for s in ksh; do
-    if [ -x $p/$s ]; then
-      ls -l $p/$s | grep -- '->' > /dev/null 2>&1
-      rc1=$?
-      ls -l $p/$s | grep '/etc/alternatives' > /dev/null 2>&1
-      rc2=$?
-      if [ $rc1 -ne 0 -o $rc2 -eq 0 ]; then
-        cmd="$p/$s -c \". $RUNTOPDIR/shellfuncs.sh;getshelltype;echo \\\$shell\""
-        tshell=`eval $cmd`
-        case $tshell in
-          pdksh)
-            ;;
-          *)
-            echo ${EN} "${s} ${EC}" >&3
-            echo "   testing with ${s} "
-            $p/$s -n $RUNTOPDIR/mkconfig.sh
-            rc=$?
-            if [ $rc -ne 0 ];then grc=$rc; fi
-            ;;
-        esac
-      fi
-    fi
-  done
+getlistofshells
+for s in $shelllist; do
+  cmd="$s -c \". $MKCONFIG_DIR/shellfuncs.sh;getshelltype;echo \\\$shell\""
+  ss=`eval $cmd`
+  echo ${EN} "${ss} ${EC}" >&3
+  echo "   testing with ${s} "
+  $s -n $MKCONFIG_DIR/mkconfig.sh
+  rc=$?
+  if [ $rc -ne 0 ];then grc=$rc; fi
 done
 
 exit $grc
--- tests.d/compsh.sh
+++ tests.d/compsh.sh
@@ -3,62 +3,22 @@
 script=$@
 echo ${EN} "compile mkconfig units${EC}" >&3
 
+. $MKCONFIG_DIR/shellfuncs.sh
 grc=0
 
+cd $MKCONFIG_DIR/mkconfig.units
+
 echo ${EN} " ${EC}" >&3
-
-cd $RUNTOPDIR/mkconfig.units
-
-plist=""
-ls -ld /bin | grep -- '->' > /dev/null 2>&1
-if [ $? -ne 0 ]; then
-  plist=/bin
-fi
-plist="${plist} /usr/bin /usr/local/bin"
-
-for p in $plist; do
-  for s in sh bash posh ash dash mksh; do
-    if [ -x $p/$s ]; then
-      ls -l $p/$s | grep -- '->' > /dev/null 2>&1
-      rc1=$?
-      ls -l $p/$s | grep '/etc/alternatives' > /dev/null 2>&1
-      rc2=$?
-      if [ $rc1 -ne 0 -o $rc2 -eq 0 ]; then
-        echo ${EN} "${s} ${EC}" >&3
-        echo "   testing with ${s} "
-        for f in *.sh; do
-          $p/$s -n $f
-          rc=$?
-          if [ $rc -ne 0 ];then grc=$rc; fi
-        done
-      fi
-    fi
-  done
-
-  for s in ksh; do
-    if [ -x $p/$s ]; then
-      ls -l $p/$s | grep -- '->' > /dev/null 2>&1
-      rc1=$?
-      ls -l $p/$s | grep '/etc/alternatives' > /dev/null 2>&1
-      rc2=$?
-      if [ $rc1 -ne 0 -o $rc2 -eq 0 ]; then
-        cmd="$p/$s -c \". $RUNTOPDIR/shellfuncs.sh;getshelltype;echo \\\$shell\""
-        tshell=`eval $cmd`
-        case $tshell in
-          pdksh)
-            ;;
-          *)
-            echo ${EN} "${s} ${EC}" >&3
-            echo "   testing with ${s} "
-            for f in *.sh; do
-              $p/$s -n $f
-              rc=$?
-              if [ $rc -ne 0 ];then grc=$rc; fi
-            done
-            ;;
-        esac
-      fi
-    fi
+getlistofshells
+for s in $shelllist; do
+  cmd="$s -c \". $MKCONFIG_DIR/shellfuncs.sh;getshelltype;echo \\\$shell\""
+  ss=`eval $cmd`
+  echo ${EN} "${ss} ${EC}" >&3
+  echo "   testing with ${s} "
+  for f in *.sh; do
+    $s -n $f
+    rc=$?
+    if [ $rc -ne 0 ];then grc=$rc; fi
   done
 done
 
