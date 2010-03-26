b1e73f02d0071f48a7af5717ae3a6b5404659dc6
bll <none@none>
2010-03-25 17:58:33 -0700

    Initial revision

---
+++ tests.d/multword.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+script=$@
+echo ${EN} "output multiple words${EC}" >&3
+
+. $MKCONFIG_DIR/shellfuncs.sh
+grc=0
+
+echo ${EN} " ${EC}" >&3
+getlistofshells
+for s in $shelllist; do
+  cmd="$s -c \". $MKCONFIG_DIR/shellfuncs.sh;getshelltype;echo \\\$shell\""
+  ss=`eval $cmd`
+  if [ "$ss" = "sh" ]; then
+    ss=`echo $s | sed 's,.*/,,'`
+  fi
+  echo ${EN} "${ss} ${EC}" >&3
+  echo "   testing with ${s} "
+  _MKCONFIG_SHELL=$s
+  export _MKCONFIG_SHELL
+  shell=$ss
+  rm -f mkconfig.cache multword.env mkconfig.log > /dev/null 2>&1
+  eval "$SHELL -x $MKCONFIG_DIR/mkconfig.sh -C $RUNTESTDIR/multword.dat"
+  . ./mkconfig.cache
+  if [ "$di_env_test_multword" != "word1 word2" ]; then
+    echo "   failed with ${s}: cache: $di_env_test_multword"
+    grc=1
+  fi
+  . ./multword.env
+  if [ "$test_multword" != "word1 word2" ]; then
+    echo "   failed with ${s}: env: $test_multword"
+    grc=1
+  fi
+  echo "## mkconfig.cache"
+  cat mkconfig.cache
+  echo "## multword.env"
+  cat multword.env
+  echo "## mkconfig.log"
+  cat mkconfig.log
+done
+
+exit $grc
