45953561af324ac5e975a2494e310ee802ee818e
bll <none@none>
2010-05-02 20:17:02 -0700

    Initial revision

---
+++ tests.d/compmkrl.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+script=$@
+echo ${EN} "compile mkreqlib.sh${EC}" >&3
+
+. $_MKCONFIG_DIR/shellfuncs.sh
+grc=0
+
+echo ${EN} " ${EC}" >&3
+getlistofshells
+for s in $shelllist; do
+  unset _shell
+  unset shell
+  cmd="$s -c \". $_MKCONFIG_DIR/shellfuncs.sh;getshelltype;echo \\\$shell\""
+  ss=`eval $cmd`
+  if [ "$ss" = "sh" ]; then
+    ss=`echo $s | sed 's,.*/,,'`
+  fi
+  echo ${EN} "${ss} ${EC}" >&3
+  echo "   testing with ${s} "
+  $s -n $_MKCONFIG_DIR/mkreqlib.sh
+  rc=$?
+  if [ $rc -ne 0 ];then grc=$rc; fi
+done
+
+exit $grc
