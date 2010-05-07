14778a97acc3e65d477397e850b6be1051bb2c4b
bll <none@none>
2010-05-07 07:53:49 -0700

    test w/all shells fix test

--- tests.d/c.d/c-cache.sh
+++ tests.d/c.d/c-cache.sh
@@ -2,19 +2,71 @@
 
 script=$@
 
-echo ${EN} "cache${EC}" >&3
-eval "${script} -C $RUNTESTDIR/cache.dat"
-mv -f cache.ctest cache.a
-mv -f mkconfig_c.vars cache.a.vars
-eval "${script} $RUNTESTDIR/cache.dat"
-mv -f cache.ctest cache.b
-mv -f mkconfig_c.vars cache.b.vars
-grc=0
-echo "## diff config.h"
-diff -b cache.a cache.b
-if [ $? -ne 0 ]; then grc=$rc; fi
-echo "## diff vars"
-diff -b cache.a.vars cache.b.vars
-if [ $? -ne 0 ]; then grc=$rc; fi
-exit $grc
+. $_MKCONFIG_DIR/shellfuncs.sh
+testshcapability
 
+count=1
+while test -f cache.${count}; do
+  domath count "$count + 1"
+done
+
+if [ $count -eq 1 ]; then
+  echo ${EN} "cache${EC}" >&3
+  pwd
+  eval "${script} -C $RUNTESTDIR/cache.dat"
+  mv -f cache.ctest cache.${count}
+  cp -f mkconfig_c.vars cache.${count}.vars
+  pwd
+  $0 $@
+  exit $?
+fi
+
+dosh=T
+case $script in
+  *.pl)
+    dosh=F
+    ;;
+esac
+
+if [ $dosh = "T" ]; then
+  getlistofshells
+  echo ${EN} " ${EC}" >&3
+  for s in $shelllist; do
+    unset _shell
+    unset shell
+    cmd="$s -c \". $_MKCONFIG_DIR/shellfuncs.sh;getshelltype;echo \\\$shell\""
+    ss=`eval $cmd`
+    cmd="$s -c \". $_MKCONFIG_DIR/shellfuncs.sh;getshelltype;echo \\\$shell\""
+    ss=`eval $cmd`
+    if [ "$ss" = "sh" ]; then
+      ss=`echo $s | sed 's,.*/,,'`
+    fi
+    echo ${EN} "${ss} ${EC}" >&3
+    eval "${s} -c '${script} $RUNTESTDIR/cache.dat'"
+    mv -f cache.ctest cache.${count}
+    mv -f mkconfig_c.vars cache.${count}.vars
+    cp -f cache.1.vars mkconfig_c.vars
+    domath count "$count + 1"
+  done
+else
+  echo ${EN} "cache${EC}" >&3
+  eval "${script} $RUNTESTDIR/cache.dat"
+  mv -f cache.ctest cache.${count}
+  mv -f mkconfig_c.vars cache.${count}.vars
+  cp -f cache.1.vars mkconfig_c.vars
+fi
+
+grc=0
+c=2
+while test $c -lt $count; do
+  echo "## diff config.h 1 ${c}"
+  diff -b cache.1 cache.${c}
+  rc=$?
+  if [ $rc -ne 0 ]; then grc=$rc; fi
+  echo "## diff vars 1 $c"
+  diff -b cache.1.vars cache.${c}.vars
+  rc=$?
+  if [ $? -ne 0 ]; then grc=$rc; fi
+  domath c "$c + 1"
+done
+exit $grc
