b049e27292f0109d6fda6e48ad5f79e4e122b8bb
bll <none@none>
2010-05-11 11:57:54 -0700

    new directory structure

--- runtests.sh
+++ runtests.sh
@@ -10,29 +10,25 @@
 # speed at the cost of maintainability...
 # File Descriptors:
 #    9 - $TSTRUNLOG
+#    8 - $MAINLOG
 #    3 - stdout (as 1 is directed to the log)
 #
 
 DOPERL=T
 TESTORDER=test_order
-RUNTMP=_tmp_runtests
-export RUNTMP
 
-RUNTOPDIR=`pwd`
-export RUNTOPDIR
+_MKCONFIG_RUNTOPDIR=`pwd`
+export _MKCONFIG_RUNTOPDIR
 mypath=`echo $0 | sed -e 's,/[^/]*$,,'`
 cd $mypath
 _MKCONFIG_DIR=`pwd`
 export _MKCONFIG_DIR
-cd $RUNTOPDIR
+cd $_MKCONFIG_RUNTOPDIR
 . ${_MKCONFIG_DIR}/shellfuncs.sh
 
 doshelltest $0 $@
 setechovars
 mkconfigversion
-
-RUNLOG=$RUNTOPDIR/tests.log
-TSTRUNLOG=$RUNTOPDIR/test_tmp.log
 
 unset GREP_OPTIONS
 unset DI_ARGS
@@ -63,10 +59,10 @@ if [ $? != 0 ]; then
   exit 1
 fi
 
-RUNTESTDIR=`pwd`
-export RUNTESTDIR
-RUNTMPDIR="$RUNTESTDIR/$RUNTMP"
-export RUNTMPDIR
+_MKCONFIG_RUNTESTDIR=`pwd`
+export _MKCONFIG_RUNTESTDIR
+_MKCONFIG_RUNTMPDIR=$_MKCONFIG_RUNTOPDIR/_mkconfig_runtests
+export _MKCONFIG_RUNTMPDIR
 
 if [ "$teststorun" = "" ]; then
   if [ ! -f "$TESTORDER" ]; then
@@ -76,11 +72,13 @@ if [ "$teststorun" = "" ]; then
   fi
 fi
 
-test -d "$RUNTMP" && rm -rf "$RUNTMP"
-mkdir $RUNTMP
-> $RUNLOG
+test -d "$_MKCONFIG_RUNTMPDIR" && rm -rf "$_MKCONFIG_RUNTMPDIR"
+mkdir -p $_MKCONFIG_RUNTMPDIR
 
-getlistofshells
+MAINLOG=${_MKCONFIG_RUNTMPDIR}/main.log
+> $MAINLOG
+exec 8>>$MAINLOG
+getlistofshells >&8
 export shelllist
 count=0
 fcount=0
@@ -98,9 +96,11 @@ for tbase in $teststorun; do
   ok=T
   if [ ! -f ./$tf ]; then
     echo "$tf ... missing ... failed"
+    echo "$tf ... missing ... failed" >&8
     ok=F
   elif [ ! -x ./$tf ]; then
     echo "$tf ... permission denied ... failed"
+    echo "$tf ... permission denied ... failed" >&8
     ok=F
   fi
   if [ $ok = F ]; then
@@ -109,109 +109,110 @@ for tbase in $teststorun; do
     continue
   fi
 
+  dt=`date`
+  arg=""
+  suffix=""
+  if [ -f $tmkconfig ]; then
+    arg="mkconfig.sh"
+    suffix="_sh"
+  fi
+
+  _MKCONFIG_TSTRUNTMPDIR=$_MKCONFIG_RUNTMPDIR/${tbase}${suffix}
+  export _MKCONFIG_TSTRUNTMPDIR
+  mkdir -p ${_MKCONFIG_TSTRUNTMPDIR}
+
+  TSTRUNLOG=${_MKCONFIG_TSTRUNTMPDIR}/${tbase}.log
   > $TSTRUNLOG
   exec 9>>$TSTRUNLOG
-  dt=`date`
+
   echo "####" >&9
   echo "# Test: $tf $arg" >&9
   echo "# $dt" >&9
   echo "####" >&9
   grc=0
-  arg=""
-  if [ -f $tmkconfig ]; then
-    echo "##==  mkconfig.sh " >&9
-    arg="mkconfig.sh"
-  fi
   echo ${EN} "$tf ... ${arg} ${EC}"
+  echo ${EN} "$tf ... ${arg} ${EC}" >&8
   if [ -f $tconfig ]; then
-    cat $tconfig > $RUNTMP/$tconfh
+    cp $tconfig $_MKCONFIG_TSTRUNTMPDIR/$tconfh
   fi
   if [ "$arg" != "" ]; then
     arg="$_MKCONFIG_DIR/$arg"
   fi
 
-  cd $RUNTMP
-  echo "##== stdout" >&9
-  ../$tf $arg 3>&1 >&9 2>&1
+  cd $_MKCONFIG_TSTRUNTMPDIR
+  $_MKCONFIG_RUNTESTDIR/$tf $arg 3>&1 >&9 2>&1
   rc=$?
-  if [ -f mkconfig.log ]; then
-    echo "##== mkconfig.log" >&9
-    cat mkconfig.log >&9
-  fi
-  cd ..
+  cd $_MKCONFIG_RUNTESTDIR
 
   dt=`date`
   echo "####" >&9
   echo "# $dt" >&9
+  echo "# exit $rc" >&9
   echo "####" >&9
+  exec 9>&-
   if [ $rc -ne 0 ]; then
     echo " ... failed"
+    echo " failed" >&8
     domath fcount "$fcount + 1"
     grc=1
-    cat $TSTRUNLOG >> $RUNLOG
   else
     echo " ... success"
+    echo " success" >&8
   fi
-  if [ "$DI_KEEP_RUN_LOG" != "" ]; then
-    cat $TSTRUNLOG >> $RUNLOG
-  fi
-  rm -f $TSTRUNLOG
   domath count "$count + 1"
 
   if [ "$DOPERL" = "T" -a -f $tmkconfig ]; then
+    _MKCONFIG_TSTRUNTMPDIR=$_MKCONFIG_RUNTMPDIR/${tbase}_pl
+    export _MKCONFIG_TSTRUNTMPDIR
+    mkdir -p ${_MKCONFIG_TSTRUNTMPDIR}
+    TSTRUNLOG=$_MKCONFIG_TSTRUNTMPDIR/${tbase}.log
     > $TSTRUNLOG
+    exec 9>>$TSTRUNLOG
     dt=`date`
     echo "####" >&9
     echo "# Test: $tf mkconfig.pl" >&9
     echo "# $dt" >&9
     echo "####" >&9
     echo ${EN} "$tf ... mkconfig.pl ${EC}"
-    echo "##== mkconfig.pl " >&9
+    echo ${EN} "$tf ... mkconfig.pl ${EC}" >&8
+    echo "## Using mkconfig.pl " >&9
     if [ -f $tconfig ]; then
-      cat $tconfig > $RUNTMP/$tconfh
+      cp $tconfig $_MKCONFIG_TSTRUNTMPDIR/$tconfh
     fi
 
-    cd $RUNTMP
-    echo "##== stdout" >&9
-    ../$tf perl $_MKCONFIG_DIR/mkconfig.pl 3>&1 >&9 2>&1
+    cd $_MKCONFIG_TSTRUNTMPDIR
+    $_MKCONFIG_RUNTESTDIR/$tf perl $_MKCONFIG_DIR/mkconfig.pl 3>&1 >&9 2>&1
     rc=$?
-    if [ -f mkconfig.log ]; then
-      echo "##== mkconfig.log" >&9
-      cat mkconfig.log >&9
-    fi
-    cd ..
+    cd $_MKCONFIG_RUNTESTDIR
 
     dt=`date`
     echo "####" >&9
     echo "# $dt" >&9
+    echo "# exit $rc" >&9
     echo "####" >&9
     exec 9>&-
     if [ $rc -ne 0 ]; then
       echo " ... failed"
+      echo " failed" >&8
       domath fcount "$fcount + 1"
-      cat $TSTRUNLOG >> $RUNLOG
     else
       echo " ... success"
+      echo " success" >&8
     fi
-    if [ "$DI_KEEP_RUN_LOG" != "" ]; then
-      cat $TSTRUNLOG >> $RUNLOG
-    fi
-    rm -f $TSTRUNLOG
     domath count "$count + 1"
   fi
 done
 
-if [ $fcount -eq 0 ]; then
-  if [ "$DI_KEEP_RUN_LOG" = "" ]; then
-    rm -f $RUNLOG
-  fi
-fi
+exec 8>&-
+
 if [ $count -eq 0 ]; then  # this can't be right...
   $fcount = -1
 fi
 
 echo "$count tests $fcount failures"
-if [ "$DI_KEEP_RUN_TMP" = "" ]; then
-  test -d "$RUNTMP" && rm -rf "$RUNTMP"
+if [ $fcount -eq 0 ]; then
+  if [ "$DI_KEEP_RUN_TMP" = "" ]; then
+    test -d "$_MKCONFIG_RUNTMPDIR" && rm -rf "$_MKCONFIG_RUNTMPDIR"
+  fi
 fi
 exit $fcount
