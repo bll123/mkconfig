5a161e5d1c8af6163f20333753dcb268ad29ea77
bll <none@none>
2010-05-12 21:42:22 -0700

    uuoc

--- tests.d/c.d/c-include.sh
+++ tests.d/c.d/c-include.sh
@@ -5,8 +5,7 @@ script=$@
 echo ${EN} "include${EC}" >&5
 eval "${script} -C ${_MKCONFIG_RUNTESTDIR}/include.dat"
 echo "## diff include.ctmp include.ctest"
-cat include.ctest |
-    sed -e '/^#define _key_/d' -e '/^#define _proto_/d' > t
+sed -e '/^#define _key_/d' -e '/^#define _proto_/d' include.ctest > t
 mv t include.ctest
 diff -b include.ctmp include.ctest
 rc=$?
