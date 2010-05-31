68c007fc56cc74ffdc5d592c5fe5752ee76100a1
bll <none@none>
2010-05-31 11:06:01 -0700

    always print add check_include_quota

--- units/c-include-conflict.sh
+++ units/c-include-conflict.sh
@@ -27,10 +27,11 @@ check_include_time () {
     fi
 
     trc=0
+    printlabel $name "header: include both time.h & sys/time.h"
+
     getdata _hdr_time ${_MKCONFIG_PREFIX} _hdr_time
     getdata _sys_time ${_MKCONFIG_PREFIX} _sys_time
     if [ "${_hdr_time}" = "time.h" -a "${_sys_time}" = "sys/time.h" ]; then
-      printlabel $name "header: include both time.h & sys/time.h"
       checkcache ${_MKCONFIG_PREFIX} $name
       if [ $rc -eq 0 ]; then return; fi
 
@@ -41,5 +42,35 @@ main () { struct tm x; }
       do_check_compile "${name}" "${code}" std
     else
       setdata ${_MKCONFIG_PREFIX} "${name}" "${trc}"
+      printyesno_val "${name}" $trc ""
+    fi
+}
+
+check_include_quota () {
+    name="_$1"
+
+    if [ "${CC}" = "" ]; then
+      echo "No compiler specified" >&2
+      return
+    fi
+
+    trc=0
+    printlabel $name "header: include both sys/quota.h & linux/quota.h"
+
+    getdata _hdr_linux_quota ${_MKCONFIG_PREFIX} _hdr_linux_quota
+    getdata _sys_quota ${_MKCONFIG_PREFIX} _sys_quota
+    if [ "${_hdr_linux_quota}" = "linux/quota.h" -a \
+        "${_sys_quota}" = "sys/quota.h" ]; then
+      checkcache ${_MKCONFIG_PREFIX} $name
+      if [ $rc -eq 0 ]; then return; fi
+
+      code="#include <sys/quota.h>
+#include <linux/quota.h>
+main () { int x; return; }
+"
+      do_check_compile "${name}" "${code}" std
+    else
+      setdata ${_MKCONFIG_PREFIX} "${name}" "${trc}"
+      printyesno_val "${name}" $trc ""
     fi
 }
