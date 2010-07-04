de7b8193066fecfc3aa2f4cd85a2d90a33ef5f81
bll <none@none>
2010-07-04 12:35:29 -0700

    compile all perl scripts

--- tests.d/compperl.sh
+++ tests.d/compperl.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
 script=$@
-echo ${EN} "compile mkconfig.pl${EC}" >&5
+echo ${EN} "compile perl scripts${EC}" >&5
 
 grc=0
 
-perl -cw $_MKCONFIG_DIR/mkconfig.pl
-rc=$?
-if [ $rc -ne 0 ];then grc=$rc; fi
+cd $_MKCONFIG_DIR
+for i in *.pl; do
+  perl -cw $i
+  rc=$?
+  if [ $rc -ne 0 ];then grc=$rc; fi
+done
 
 exit $grc
