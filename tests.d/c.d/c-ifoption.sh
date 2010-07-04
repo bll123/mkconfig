453b79d32fc76525ed9525482d1fdd01f9f599a4
bll <none@none>
2010-07-03 18:04:59 -0700

    rewrite

--- tests.d/c.d/c-ifoption.sh
+++ tests.d/c.d/c-ifoption.sh
@@ -6,7 +6,7 @@ script=$@
 . $_MKCONFIG_DIR/shellfuncs.sh
 testshcapability
 
-echo ${EN} "option_env${EC}" >&5
+echo ${EN} "ifoption${EC}" >&5
 grc=0
 count=1
 
@@ -18,8 +18,9 @@ case $script in
     ;;
 esac
 
-OPTIONS="NO_TEST_NO YES_TEST_YES WITHOUT_TEST_WITHOUT WITH_TEST_WITH
-    TEST_OPT_T=T TEST_OPT_F=F TEST_F_OTHER=F"
+OPTIONS="WITH_TEST_WITH WITHOUT_TEST_WITHOUT
+    ENABLE_TEST_ENABLE DISABLE_TEST_DISABLE
+    TEST_ASSIGN_T=T TEST_ASSIGN_F=F"
 export OPTIONS
 
 if [ "$dosh" = "T" ]; then
@@ -38,26 +39,28 @@ if [ "$dosh" = "T" ]; then
     export _MKCONFIG_SHELL
     shell=$ss
 
-    eval "${s} ${script} -C ${_MKCONFIG_RUNTESTDIR}/option_env.dat"
-    for t in TEST_T TEST_YES TEST_WITH TEST_OPT_T; do
-      echo "chk: _option_$t (1)"
-      grep "^_option_${t}=\"1\"$" option_env.ctest
+    eval "${s} ${script} -C ${_MKCONFIG_RUNTESTDIR}/ifoption_env.dat"
+    for t in \
+        _test_with _test_without \
+        _test_enable _test_disable \
+        _test_assign_t _test_assign_f \
+        _test_else_with _test_else_without \
+        _test_else_enable _test_else_disable \
+        _test_else_assign_t _test_else_assign_f \
+        _test_neg_with _test_neg_without \
+        _test_neg_enable _test_neg_disable \
+        _test_neg_assign_t _test_neg_assign_f \
+        _test_else_neg_with _test_else_neg_without \
+        _test_else_neg_enable _test_else_neg_disable \
+        _test_else_neg_assign_t _test_else_neg_assign_f \
+        _test_a _test_b _test_c _test_d _test_e _test_f _test_g \
+        _test_h _test_i _test_j _test_k _test_l _test_m _test_n; do
+      echo "chk: $t (1)"
+      grep "^${t}=\"1\"$" ifoption_env.ctest
       rc=$?
       if [ $rc -ne 0 ]; then grc=$rc; fi
     done
-    for t in TEST_F TEST_NO TEST_WITHOUT TEST_OPT_F TEST_F_OTHER; do
-      echo "chk: _option_$t (0)"
-      grep "^_option_${t}=\"0\"$" option_env.ctest
-      rc=$?
-      if [ $rc -ne 0 ]; then grc=$rc; fi
-    done
-    for t in TEST_TEMP; do
-      echo "chk: _option_$t (0)"
-      grep "^_option_${t}=\"0\"$" option_env.ctest
-      rc=$?
-      if [ $rc -ne 0 ]; then grc=$rc; fi
-    done
-    mv option_env.ctest option_env.ctest.${count}
+    mv ifoption_env.ctest ifoption_env.ctest.${count}
     mv mkconfig.log mkconfig.log.${count}
     mv mkconfig.cache mkconfig.cache.${count}
     mv mkconfig_env.vars mkconfig_env.vars.${count}
