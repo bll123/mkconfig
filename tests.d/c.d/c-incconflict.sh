f03081874112915bd1a3754cd2c62996e6ab6cf3
bll <none@none>
2010-07-10 15:36:58 -0700

    Initial revision

---
+++ tests.d/c.d/c-incconflict.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+if [ "$1" = "-d" ]; then
+  echo ${EN} " include conflict${EC}"
+  exit 0
+fi
+
+script=$@
+
+grc=0
+
+CFLAGS="-I${_MKCONFIG_TSTRUNTMPDIR} ${CFLAGS}"
+LDFLAGS="-L${_MKCONFIG_TSTRUNTMPDIR} ${LDFLAGS}"
+export CFLAGS LDFLAGS
+
+> incconf1.h echo '
+extern int incconf1 _ARG_((int));
+'
+> incconf2.h echo '
+extern int incconf1 _ARG_((char *));
+'
+> incconf3.h echo '
+extern int incconf1 _ARG_((int));
+'
+
+${script} -C ${_MKCONFIG_RUNTESTDIR}/incconflict.dat
+grep "^#define _inc_conflict__hdr_incconf1__hdr_incconf2 0$" incconflict.ctest
+rc=$?
+if [ $rc -ne 0 ]; then grc=$rc; fi
+grep "^#define _inc_conflict__hdr_incconf1__hdr_incconf3 1$" incconflict.ctest
+rc=$?
+if [ $rc -ne 0 ]; then grc=$rc; fi
+
+if [ "$stag" != "" ]; then
+  mv incconflict.ctest incconflict.ctest${stag}
+  mv mkconfig.log mkconfig.log${stag}
+  mv mkconfig.cache mkconfig.cache${stag}
+  mv mkconfig_c.vars mkconfig_c.vars${stag}
+fi
+
+exit $grc
