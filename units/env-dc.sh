0e0a3858497951f30c560126d42be2b373daec70
bll <none@none>
2010-12-19 14:40:33 -0800

    remove stuff that's in env-cc

--- units/env-dc.sh
+++ units/env-dc.sh
@@ -19,33 +19,6 @@
 require_unit env-main
 require_unit env-systype
 
-env_dogetconf=F
-
-_dogetconf () {
-  if [ "$env_dogetconf" = T ]; then
-    return
-  fi
-
-  locatecmd xgetconf getconf
-  if [ "${xgetconf}" != "" ]
-  then
-      echo "using flags from getconf" >&9
-      lfdflags="`${xgetconf} LFS_CFLAGS 2>/dev/null`"
-      if [ "$lfdflags" = "undefined" ]; then
-        lfdflags=""
-      fi
-      lfldflags="`${xgetconf} LFS_LDFLAGS 2>/dev/null`"
-      if [ "$lfldflags" = "undefined" ]; then
-        lfldflags=""
-      fi
-      lflibs="`${xgetconf} LFS_LIBS 2>/dev/null`"
-      if [ "$lflibs" = "undefined" ]; then
-        lflibs=""
-      fi
-  fi
-  env_dogetconf=T
-}
-
 check_dc () {
   DC=${DC:-gdc}
 
@@ -128,84 +101,4 @@ check_dflags () {
 
   printyesno_val DFLAGS "$dflags"
   setdata ${_MKCONFIG_PREFIX} DFLAGS "$dflags"
-}
-
-check_ldflags () {
-  ldflags="${LDFLAGS:-}"
-
-  printlabel LDFLAGS "D Load flags"
-
-  _dogetconf
-
-  TDC=${DC}
-  if [ "${_MKCONFIG_USING_GCC}" = "Y" ]
-  then
-    TDC=gcc
-  fi
-
-  case ${_MKCONFIG_SYSTYPE} in
-      FreeBSD)
-        # FreeBSD has many packages that get installed in /usr/local
-        ldflags="-L/usr/local/lib $ldflags"
-        ;;
-      HP-UX)
-        _dohpflags
-        ldflags="$hpldflags $ldflags"
-        case ${TDC} in
-          cc)
-            ldflags="-Wl,+s $ldflags"
-            ;;
-        esac
-        ;;
-      OS/2)
-        ldflags="-Zexe"
-        ;;
-      SunOS)
-        case ${_MKCONFIG_SYSREV} in
-          5.*)
-            case ${TDC} in
-              cc)
-                echo $CFLAGS | grep -- '-xO3' >/dev/null 2>&1
-                case $rc in
-                    0)
-                        ldflags="-fast $ldflags"
-                        ;;
-                esac
-                ;;
-            esac
-            ;;
-        esac
-        ;;
-  esac
-
-  ldflags="$ldflags $lfldflags"
-
-  echo "ldflags:${ldflags}" >&9
-
-  printyesno_val LDFLAGS "$ldflags"
-  setdata ${_MKCONFIG_PREFIX} LDFLAGS "$ldflags"
-}
-
-check_libs () {
-  libs="${LIBS:-}"
-
-  printlabel LIBS "D Libraries"
-
-  _dogetconf
-
-  gdcflags=""
-
-  TDC=${DC}
-  if [ "${_MKCONFIG_USING_GDC}" = "Y" ]
-  then
-    TDC=gdc
-  fi
-
-  # largefile flags
-  libs="$libs $lflibs"
-
-  echo "libs:${libs}" >&9
-
-  printyesno_val LIBS "$libs"
-  setdata ${_MKCONFIG_PREFIX} LIBS "$libs"
 }
