2afbb0f6306a57cf58efa347b977fc2be25e2881
bll <none@none>
2010-12-19 14:36:43 -0800

    more tests

--- tests.d/d.d/d-cstruct.sh
+++ tests.d/d.d/d-cstruct.sh
@@ -33,21 +33,29 @@ struct a
   long double ld;
   double d;
   long long  ll;
+  signed long long  sgll;
   long long int  lli;
+  signed long long int  sglli;
   unsigned long long  ull;
   unsigned long long int ulli;
   float f;
   long  l;
+  signed long  sgl;
   unsigned long  ul;
   long int li;
+  signed long int sgli;
   unsigned long int uli;
   int   i;
+  signed int   sgi;
   unsigned int   ui;
   short s;
+  signed short sgs;
   short int si;
+  signed short int sgsi;
   unsigned short us;
   unsigned short int usi;
   char  c;
+  signed char  sgc;
   unsigned char  uc;
   char  carr [20];
 };
@@ -90,6 +98,40 @@ struct i {
   char i;
 };
 
+struct j { int j; };
+
+typedef struct k { int k; } k_t;
+
+typedef struct { int l; } l_t;
+
+struct m {
+ int m;
+ union {
+   int a;
+   long b;
+ };
+} m_t;
+
+struct { int n; union { int a; long b; }; int n2; } n_t;
+
+/* forward dcl */
+struct o;
+
+struct o {
+  int o;
+};
+
+/* forward dcl */
+struct p;
+
+/* forward dcl */
+struct q;
+
+struct q {
+  int q;
+  struct q *qq;
+};
+
 #endif
 
 _HERE_
@@ -97,59 +139,21 @@ _HERE_
 ${_MKCONFIG_SHELL} ${script} -d `pwd` -C ${_MKCONFIG_RUNTESTDIR}/d-cstruct.dat
 grc=0
 
-grep -l "^enum bool _cstruct_a = true;$" cstruct.d > /dev/null 2>&1
-rc=$?
-if [ $rc -ne 0 ]; then
-  grc=1
-fi
+for x in a b c d e f g h i j k l m n o q; do
+  grep -l "^enum bool _cstruct_${x} = true;$" cstruct.d > /dev/null 2>&1
+  rc=$?
+  if [ $rc -ne 0 ]; then
+    grc=1
+  fi
+done
 
-grep -l "^enum bool _cstruct_b = true;$" cstruct.d > /dev/null 2>&1
-rc=$?
-if [ $rc -ne 0 ]; then
-  grc=1
-fi
-
-grep -l "^enum bool _cstruct_c = true;$" cstruct.d > /dev/null 2>&1
-rc=$?
-if [ $rc -ne 0 ]; then
-  grc=1
-fi
-
-grep -l "^enum bool _cstruct_d = true;$" cstruct.d > /dev/null 2>&1
-rc=$?
-if [ $rc -ne 0 ]; then
-  grc=1
-fi
-
-grep -l "^enum bool _cstruct_e = true;$" cstruct.d > /dev/null 2>&1
-rc=$?
-if [ $rc -ne 0 ]; then
-  grc=1
-fi
-
-grep -l "^enum bool _cstruct_f = true;$" cstruct.d > /dev/null 2>&1
-rc=$?
-if [ $rc -ne 0 ]; then
-  grc=1
-fi
-
-grep -l "^enum bool _cstruct_g = true;$" cstruct.d > /dev/null 2>&1
-rc=$?
-if [ $rc -ne 0 ]; then
-  grc=1
-fi
-
-grep -l "^enum bool _cstruct_h = true;$" cstruct.d > /dev/null 2>&1
-rc=$?
-if [ $rc -ne 0 ]; then
-  grc=1
-fi
-
-grep -l "^enum bool _cstruct_i = true;$" cstruct.d > /dev/null 2>&1
-rc=$?
-if [ $rc -ne 0 ]; then
-  grc=1
-fi
+for x in p; do
+  grep -l "^enum bool _cstruct_${x} = false;$" cstruct.d > /dev/null 2>&1
+  rc=$?
+  if [ $rc -ne 0 ]; then
+    grc=1
+  fi
+done
 
 if [ $grc -eq 0 ]; then
   ${DC} -c ${DFLAGS} cstruct.d
