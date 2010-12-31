dd70cfbce3a356348c7c95b654123308c8eea276
bll <none@none>
2010-12-30 17:06:21 -0800

    fix sed for rquota xdr add gqa_uid xdr

--- units/c-rquota.sh
+++ units/c-rquota.sh
@@ -21,7 +21,7 @@ require_unit c-main
 check_rquota_xdr () {
     name="_$1"
 
-    printlabel $name "rquota type"
+    printlabel $name "rquota xdr"
     checkcache_val ${_MKCONFIG_PREFIX} $name
     if [ $rc -eq 0 ]; then return; fi
 
@@ -40,12 +40,55 @@ check_rquota_xdr () {
       rc=$?
 
       if [ $rc -eq 0 ]; then
-        egrep -l '[	 ][	 ]*rq_bhardlimit[	 ]*;' $name.out >&9 2>&1
+        sed '1,/struct *rquota *{/d' $name.out |
+          egrep '[	 ][	 ]*rq_bhardlimit[	 ]*;' >&9 2>&1
         rc=$?
         if [ $rc -eq 0 -a "$val" != "" ]; then
-          rval=`egrep '[	 ][	 ]*rq_bhardlimit[	 ]*;' $name.out |
+          rval=`sed '1,/struct *rquota *{/d' $name.out |
+            egrep '[	 ][	 ]*rq_bhardlimit[	 ]*;' |
             sed -e 's/[	 ][	 ]*rq_bhardlimit.*//' \
-            -e 's/^[	 ]*//'`
+              -e 's/^[	 ]*//'`
+          setdata ${_MKCONFIG_PREFIX} "${name}" "xdr_${rval}"
+          printyesno_val "${name}" "xdr_${rval}" ""
+          return
+        fi
+      fi
+    fi
+
+    setdata ${_MKCONFIG_PREFIX} "${name}" 0
+    printyesno_val "${name}" 0 ""
+}
+
+check_gqa_uid_xdr () {
+    name="_$1"
+
+    printlabel $name "gqa_uid xdr"
+    checkcache_val ${_MKCONFIG_PREFIX} $name
+    if [ $rc -eq 0 ]; then return; fi
+
+    getdata hdr ${_MKCONFIG_PREFIX} _hdr_rpcsvc_rquota
+    if [ "$hdr" = 0 ]; then
+      setdata ${_MKCONFIG_PREFIX} "${name}" 0
+      printyesno_val "${name}" 0 ""
+      return
+    fi
+
+    echo "header: $hdr" >&9
+    code=""
+    if [ "$hdr" != 0 ]; then
+      code="#include <$hdr>"
+      _c_chk_cpp $name "$code"
+      rc=$?
+
+      if [ $rc -eq 0 ]; then
+        sed '1,/struct *getquota_args *{/d' $name.out |
+          egrep '[	 ][	 ]*gqa_uid[	 ]*;' >&9 2>&1
+        rc=$?
+        if [ $rc -eq 0 -a "$val" != "" ]; then
+          rval=`sed '1,/struct *getquota_args *{/d' $name.out |
+            egrep '[	 ][	 ]*gqa_uid[	 ]*;' |
+            sed -e 's/[	 ][	 ]*gqa_uid.*//' \
+              -e 's/^[	 ]*//'`
           setdata ${_MKCONFIG_PREFIX} "${name}" "xdr_${rval}"
           printyesno_val "${name}" "xdr_${rval}" ""
           return
