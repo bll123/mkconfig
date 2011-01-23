b7c770b24d09c30b376ad851998696b7fd6c0ab0
bll <none@none>
2011-01-22 18:14:05 -0800

    rewrite; rename

--- tests.d/d.d/d-ctype.sh
+++ tests.d/d.d/d-ctype.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "$1" = "-d" ]; then
-  echo ${EN} " c-type extraction${EC}"
+  echo ${EN} " c-typeconv size${EC}"
   exit 0
 fi
 
@@ -15,8 +15,8 @@ shift
 script=$@
 
 ${_MKCONFIG_SHELL} ${_MKCONFIG_DIR}/mkconfig.sh -d `pwd` \
-    -C $_MKCONFIG_RUNTESTDIR/d-ctype.env.dat
-. ./ctype.env
+    -C $_MKCONFIG_RUNTESTDIR/d-ctypeconv.env.dat
+. ./ctypeconv.env
 
 grc=0
 
@@ -25,9 +25,9 @@ DFLAGS="-I${_MKCONFIG_TSTRUNTMPDIR} ${DFLAGS}"
 LDFLAGS="-L${_MKCONFIG_TSTRUNTMPDIR} ${LDFLAGS}"
 export CFLAGS DFLAGS LDFLAGS
 
-cat > tst4hdr.h << _HERE_
-#ifndef _INC_TST4HDR_H_
-#define _INC_TST4HDR_H_
+cat > typeconvhdr.h << _HERE_
+#ifndef _INC_TYPECONVHDR_H_
+#define _INC_TYPECONVHDR_H_
 
 typedef unsigned char a;
 typedef unsigned short int b;
@@ -49,11 +49,49 @@ typedef void *o;
 #endif
 _HERE_
 
-${_MKCONFIG_SHELL} ${script} -d `pwd` -C ${_MKCONFIG_RUNTESTDIR}/d-ctype.dat
+${_MKCONFIG_SHELL} ${script} -d `pwd` -C ${_MKCONFIG_RUNTESTDIR}/d-ctypeconv.dat
 grc=0
 
-for x in a b c d e f g h i j k m o; do
-  grep -l "^enum bool _ctype_${x} = true;$" ctype.d > /dev/null 2>&1
+csiz=`grep "^enum int _csiz_char = " ctypeconv.d | sed 's/.*= //;s/;//'`
+ssiz=`grep "^enum int _csiz_short = " ctypeconv.d | sed 's/.*= //;s/;//'`
+isiz=`grep "^enum int _csiz_int = " ctypeconv.d | sed 's/.*= //;s/;//'`
+lsiz=`grep "^enum int _csiz_long = " ctypeconv.d | sed 's/.*= //;s/;//'`
+llsiz=`grep "^enum int _csiz_long_long = " ctypeconv.d | sed 's/.*= //;s/;//'`
+
+for x in a e f; do
+  grep -l "^enum int _ctypeconv_${x} = ${csiz};$" ctypeconv.d > /dev/null 2>&1
+  rc=$?
+  if [ $rc -ne 0 ]; then
+    grc=1
+  fi
+done
+
+for x in b g h; do
+  grep -l "^enum int _ctypeconv_${x} = ${ssiz};$" ctypeconv.d > /dev/null 2>&1
+  rc=$?
+  if [ $rc -ne 0 ]; then
+    grc=1
+  fi
+done
+
+for x in c i m o; do
+  grep -l "^enum int _ctypeconv_${x} = ${isiz};$" ctypeconv.d > /dev/null 2>&1
+  rc=$?
+  if [ $rc -ne 0 ]; then
+    grc=1
+  fi
+done
+
+for x in d; do
+  grep -l "^enum int _ctypeconv_${x} = ${lsiz};$" ctypeconv.d > /dev/null 2>&1
+  rc=$?
+  if [ $rc -ne 0 ]; then
+    grc=1
+  fi
+done
+
+for x in j k; do
+  grep -l "^enum int _ctypeconv_${x} = ${llsiz};$" ctypeconv.d > /dev/null 2>&1
   rc=$?
   if [ $rc -ne 0 ]; then
     grc=1
@@ -61,15 +99,15 @@ for x in a b c d e f g h i j k m o; do
 done
 
 if [ $grc -eq 0 ]; then
-  ${DC} -c ${DFLAGS} ctype.d
+  ${DC} -c ${DFLAGS} ctypeconv.d
   if [ $? -ne 0 ]; then
-    echo "compile ctype.d failed"
+    echo "compile ctypeconv.d failed"
     grc=1
   fi
 fi
 
 if [ "$stag" != "" ]; then
-  mv ctype.d ctype.d${stag}
+  mv ctypeconv.d ctypeconv.d${stag}
   mv mkconfig.log mkconfig.log${stag}
   mv mkconfig.cache mkconfig.cache${stag}
   mv mkconfig_d.vars mkconfig_d.vars${stag}
