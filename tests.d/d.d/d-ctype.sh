6502786292debce1399acb8e32574483919cd040
bll <none@none>
2011-04-17 12:45:01 -0700

    add tests for aliases remove void * test

--- tests.d/d.d/d-ctype.sh
+++ tests.d/d.d/d-ctype.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "$1" = "-d" ]; then
-  echo ${EN} " c-typeconv size${EC}"
+  echo ${EN} " c-type size${EC}"
   exit 0
 fi
 
@@ -15,8 +15,8 @@ shift
 script=$@
 
 ${_MKCONFIG_SHELL} ${_MKCONFIG_DIR}/mkconfig.sh -d `pwd` \
-    -C $_MKCONFIG_RUNTESTDIR/d-ctypeconv.env.dat
-. ./ctypeconv.env
+    -C $_MKCONFIG_RUNTESTDIR/d-ctype.env.dat
+. ./ctype.env
 
 grc=0
 
@@ -25,9 +25,9 @@ DFLAGS="-I${_MKCONFIG_TSTRUNTMPDIR} ${DFLAGS}"
 LDFLAGS="-L${_MKCONFIG_TSTRUNTMPDIR} ${LDFLAGS}"
 export CFLAGS DFLAGS LDFLAGS
 
-cat > typeconvhdr.h << _HERE_
-#ifndef _INC_TYPECONVHDR_H_
-#define _INC_TYPECONVHDR_H_
+cat > typehdr.h << _HERE_
+#ifndef _INC_TYPEHDR_H_
+#define _INC_TYPEHDR_H_
 
 typedef unsigned char a;
 typedef unsigned short int b;
@@ -38,19 +38,18 @@ typedef unsigned char f;
 typedef signed short int g;
 typedef unsigned short int h;
 typedef signed int i;
-#if __GNUC__
-__extension__ typedef void * m;
-#endif
-typedef void *o;
+typedef long int o;
 typedef float q;
 typedef double r;
+typedef unsigned int _t;
+typedef _t t;
 
 #endif
 _HERE_
 
-cat > typeconvhdr_ll.h << _HERE_
-#ifndef _INC_TYPECONVHDR_LL_H_
-#define _INC_TYPECONVHDR_LL_H_
+cat > typehdr_ll.h << _HERE_
+#ifndef _INC_TYPEHDR_LL_H_
+#define _INC_TYPEHDR_LL_H_
 
 /* long long may not be supported */
 __extension__ typedef signed long long int j;
@@ -59,9 +58,9 @@ __extension__ typedef unsigned long long int k;
 #endif
 _HERE_
 
-cat > typeconvhdr_ld.h << _HERE_
-#ifndef _INC_TYPECONVHDR_LD_H_
-#define _INC_TYPECONVHDR_LD_H_
+cat > typehdr_ld.h << _HERE_
+#ifndef _INC_TYPEHDR_LD_H_
+#define _INC_TYPEHDR_LD_H_
 
 /* long double may not be supported */
 typedef long double s;
@@ -69,117 +68,214 @@ typedef long double s;
 #endif
 _HERE_
 
-${_MKCONFIG_SHELL} ${script} -d `pwd` -C ${_MKCONFIG_RUNTESTDIR}/d-ctypeconv.dat
+${_MKCONFIG_SHELL} ${script} -d `pwd` -C ${_MKCONFIG_RUNTESTDIR}/d-ctype.dat
 grc=0
 
-csiz=`egrep "^enum (: )?int ({ )?_csiz_char = " dctypeconv.d | sed 's/.*= //;s/[ }]*;//'`
-ssiz=`egrep "^enum (: )?int ({ )?_csiz_short = " dctypeconv.d | sed 's/.*= //;s/[ }]*;//'`
-isiz=`egrep "^enum (: )?int ({ )?_csiz_int = " dctypeconv.d | sed 's/.*= //;s/[ }]*;//'`
-lsiz=`egrep "^enum (: )?int ({ )?_csiz_long = " dctypeconv.d | sed 's/.*= //;s/[ }]*;//'`
-llsiz=`egrep "^enum (: )?int ({ )?_csiz_long_long = " dctypeconv.d | sed 's/.*= //;s/[ }]*;//'`
-fsiz=`egrep "^enum (: )?int ({ )?_csiz_float = " dctypeconv.d | sed 's/.*= //;s/[ }]*;//'`
-dsiz=`egrep "^enum (: )?int ({ )?_csiz_double = " dctypeconv.d | sed 's/.*= //;s/[ }]*;//'`
-ldsiz=`egrep "^enum (: )?int ({ )?_csiz_long_double = " dctypeconv.d | sed 's/.*= //;s/[ }]*;//'`
+csiz=`egrep "^enum (: )?int ({ )?_csiz_char = " dctype.d | sed 's/.*= //;s/[ }]*;//'`
+ssiz=`egrep "^enum (: )?int ({ )?_csiz_short = " dctype.d | sed 's/.*= //;s/[ }]*;//'`
+isiz=`egrep "^enum (: )?int ({ )?_csiz_int = " dctype.d | sed 's/.*= //;s/[ }]*;//'`
+lsiz=`egrep "^enum (: )?int ({ )?_csiz_long = " dctype.d | sed 's/.*= //;s/[ }]*;//'`
+llsiz=`egrep "^enum (: )?int ({ )?_csiz_long_long = " dctype.d | sed 's/.*= //;s/[ }]*;//'`
+fsiz=`egrep "^enum (: )?int ({ )?_csiz_float = " dctype.d | sed 's/.*= //;s/[ }]*;//'`
+dsiz=`egrep "^enum (: )?int ({ )?_csiz_double = " dctype.d | sed 's/.*= //;s/[ }]*;//'`
+ldsiz=`egrep "^enum (: )?int ({ )?_csiz_long_double = " dctype.d | sed 's/.*= //;s/[ }]*;//'`
 
 for x in a e f; do
-  egrep -l ?"^enum (: )?int ({ )?_ctypeconv_${x} = ${csiz}( })?;$" dctypeconv.d > /dev/null 2>&1
+  egrep -l ?"^enum (: )?int ({ )?_ctype_${x} = ${csiz}( })?;$" dctype.d > /dev/null 2>&1
   rc=$?
   if [ $rc -ne 0 ]; then
-    echo "## test $x failed"
+    echo "## test $x failed enum chk"
+    grc=1
+  fi
+done
+
+for x in e; do
+  egrep -l ?"^alias byte _CT_${x};$" dctype.d > /dev/null 2>&1
+  rc=$?
+  if [ $rc -ne 0 ]; then
+    echo "## test $x failed alias chk"
+    grc=1
+  fi
+done
+
+for x in a f; do
+  egrep -l ?"^alias ubyte _CT_${x};$" dctype.d > /dev/null 2>&1
+  rc=$?
+  if [ $rc -ne 0 ]; then
+    echo "## test $x failed alias chk"
     grc=1
   fi
 done
 
 for x in b g h; do
-  egrep -l "^enum (: )?int ({ )?_ctypeconv_${x} = ${ssiz}( })?;$" dctypeconv.d > /dev/null 2>&1
+  egrep -l "^enum (: )?int ({ )?_ctype_${x} = ${ssiz}( })?;$" dctype.d > /dev/null 2>&1
   rc=$?
   if [ $rc -ne 0 ]; then
-    echo "## test $x failed"
+    echo "## test $x failed enum chk"
     grc=1
   fi
 done
 
-if [ "${_MKCONFIG_USING_GCC}" = "Y" ]; then
-  for x in m; do
-    egrep -l "^enum (: )?int ({ )?_ctypeconv_${x} = ${isiz}( })?;$" dctypeconv.d > /dev/null 2>&1
-    rc=$?
-    if [ $rc -ne 0 ]; then
-      echo "## test $x failed"
-      grc=1
-    fi
-  done
-fi
-
-for x in c i o; do
-  egrep -l "^enum (: )?int ({ )?_ctypeconv_${x} = ${isiz}( })?;$" dctypeconv.d > /dev/null 2>&1
+for x in g; do
+  egrep -l ?"^alias short _CT_${x};$" dctype.d > /dev/null 2>&1
   rc=$?
   if [ $rc -ne 0 ]; then
-    echo "## test $x failed"
+    echo "## test $x failed alias chk"
+    grc=1
+  fi
+done
+
+for x in b h; do
+  egrep -l ?"^alias ushort _CT_${x};$" dctype.d > /dev/null 2>&1
+  rc=$?
+  if [ $rc -ne 0 ]; then
+    echo "## test $x failed alias chk"
+    grc=1
+  fi
+done
+
+for x in c i o t; do
+  egrep -l "^enum (: )?int ({ )?_ctype_${x} = ${isiz}( })?;$" dctype.d > /dev/null 2>&1
+  rc=$?
+  if [ $rc -ne 0 ]; then
+    echo "## test $x failed enum chk"
+    grc=1
+  fi
+done
+
+for x in i; do
+  egrep -l ?"^alias int _CT_${x};$" dctype.d > /dev/null 2>&1
+  rc=$?
+  if [ $rc -ne 0 ]; then
+    echo "## test $x failed alias chk"
+    grc=1
+  fi
+done
+
+for x in c t; do
+  egrep -l ?"^alias uint _CT_${x};$" dctype.d > /dev/null 2>&1
+  rc=$?
+  if [ $rc -ne 0 ]; then
+    echo "## test $x failed alias chk"
+    grc=1
+  fi
+done
+
+for x in d o; do
+  egrep -l "^enum (: )?int ({ )?_ctype_${x} = ${lsiz}( })?;$" dctype.d > /dev/null 2>&1
+  rc=$?
+  if [ $rc -ne 0 ]; then
+    echo "## test $x failed enum chk"
+    grc=1
+  fi
+done
+
+for x in o; do
+  egrep -l ?"^alias int _CT_${x};$" dctype.d > /dev/null 2>&1
+  rc=$?
+  if [ $rc -ne 0 ]; then
+    echo "## test $x failed alias chk"
     grc=1
   fi
 done
 
 for x in d; do
-  egrep -l "^enum (: )?int ({ )?_ctypeconv_${x} = ${lsiz}( })?;$" dctypeconv.d > /dev/null 2>&1
+  egrep -l ?"^alias uint _CT_${x};$" dctype.d > /dev/null 2>&1
   rc=$?
   if [ $rc -ne 0 ]; then
-    echo "## test $x failed"
+    echo "## test $x failed alias chk"
     grc=1
   fi
 done
 
 if [ $llsiz -gt 0 ]; then
   for x in j k; do
-    egrep -l "^enum (: )?int ({ )?_ctypeconv_${x} = ${llsiz}( })?;$" dctypeconv.d \
+    egrep -l "^enum (: )?int ({ )?_ctype_${x} = ${llsiz}( })?;$" dctype.d \
         > /dev/null 2>&1
     rc=$?
     if [ $rc -ne 0 ]; then
-      echo "## test $x failed"
+      echo "## test $x failed enum chk"
+      grc=1
+    fi
+  done
+
+  for x in j; do
+    egrep -l ?"^alias long _CT_${x};$" dctype.d > /dev/null 2>&1
+    rc=$?
+    if [ $rc -ne 0 ]; then
+      echo "## test $x failed alias chk"
+      grc=1
+    fi
+  done
+
+  for x in k; do
+    egrep -l ?"^alias ulong _CT_${x};$" dctype.d > /dev/null 2>&1
+    rc=$?
+    if [ $rc -ne 0 ]; then
+      echo "## test $x failed alias chk"
       grc=1
     fi
   done
 fi
 
 for x in q; do
-  egrep -l "^enum (: )?int ({ )?_ctypeconv_${x} = ${fsiz}( })?;$" dctypeconv.d > /dev/null 2>&1
+  egrep -l "^enum (: )?int ({ )?_ctype_${x} = ${fsiz}( })?;$" dctype.d > /dev/null 2>&1
   rc=$?
   if [ $rc -ne 0 ]; then
-    echo "## test $x failed"
+    echo "## test $x failed enum chk"
+    grc=1
+  fi
+  egrep -l ?"^alias float _CT_${x};$" dctype.d > /dev/null 2>&1
+  rc=$?
+  if [ $rc -ne 0 ]; then
+    echo "## test $x failed alias chk"
     grc=1
   fi
 done
 
 for x in r; do
-  egrep -l "^enum (: )?int ({ )?_ctypeconv_${x} = ${dsiz}( })?;$" dctypeconv.d > /dev/null 2>&1
+  egrep -l "^enum (: )?int ({ )?_ctype_${x} = ${dsiz}( })?;$" dctype.d > /dev/null 2>&1
   rc=$?
   if [ $rc -ne 0 ]; then
-    echo "## test $x failed"
+    echo "## test $x failed enum chk"
+    grc=1
+  fi
+  egrep -l ?"^alias double _CT_${x};$" dctype.d > /dev/null 2>&1
+  rc=$?
+  if [ $rc -ne 0 ]; then
+    echo "## test $x failed alias chk"
     grc=1
   fi
 done
 
 if [ $ldsiz -gt 0 ]; then
   for x in s; do
-    egrep -l "^enum (: )?int ({ )?_ctypeconv_${x} = ${ldsiz}( })?;$" dctypeconv.d \
+    egrep -l "^enum (: )?int ({ )?_ctype_${x} = ${ldsiz}( })?;$" dctype.d \
         > /dev/null 2>&1
     rc=$?
     if [ $rc -ne 0 ]; then
-      echo "## test $x failed"
+      echo "## test $x failed enum chk"
+      grc=1
+    fi
+    egrep -l ?"^alias real _CT_${x};$" dctype.d > /dev/null 2>&1
+    rc=$?
+    if [ $rc -ne 0 ]; then
+      echo "## test $x failed alias chk"
       grc=1
     fi
   done
 fi
 
 if [ $grc -eq 0 ]; then
-  ${DC} -c ${DFLAGS} dctypeconv.d
+  ${DC} -c ${DFLAGS} dctype.d
   if [ $? -ne 0 ]; then
-    echo "## compile dctypeconv.d failed"
+    echo "## compile dctype.d failed"
     grc=1
   fi
 fi
 
 if [ "$stag" != "" ]; then
-  mv dctypeconv.d dctypeconv.d${stag}
+  mv dctype.d dctype.d${stag}
   mv mkconfig.log mkconfig.log${stag}
   mv mkconfig.cache mkconfig.cache${stag}
   mv mkconfig_d.vars mkconfig_d.vars${stag}
