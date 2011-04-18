dc765f0b11f159822484e577025727e531549985
bll <none@none>
2011-04-18 13:43:30 -0700

    rename string/hex/float

--- tests.d/d.d/d-cdefine.sh
+++ tests.d/d.d/d-cdefine.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "$1" = "-d" ]; then
-  echo ${EN} " c-define int${EC}"
+  echo ${EN} " c-define ${EC}"
   exit 0
 fi
 
@@ -15,8 +15,8 @@ shift
 script=$@
 
 ${_MKCONFIG_SHELL} ${_MKCONFIG_DIR}/mkconfig.sh -d `pwd` \
-    -C $_MKCONFIG_RUNTESTDIR/d-cdefint.env.dat
-. ./cdefint.env
+    -C $_MKCONFIG_RUNTESTDIR/d-cdefine.env.dat
+. ./cdefine.env
 
 grc=0
 
@@ -25,49 +25,152 @@ DFLAGS="-I${_MKCONFIG_TSTRUNTMPDIR} ${DFLAGS}"
 LDFLAGS="-L${_MKCONFIG_TSTRUNTMPDIR} ${LDFLAGS}"
 export CFLAGS DFLAGS LDFLAGS
 
-cat > cdefint.h << _HERE_
-#ifndef _INC_cdefint_H_
-#define _INC_cdefint_H_
+cat > cdefine.h << _HERE_
+#ifndef _INC_CDEFINE_H_
+#define _INC_CDEFINE_H_
 
 #define a 1
 #define b 2
 
+#define d "d"
+#define e "abc"
+
+#define g 255
+#define h 1024
+
+#define pi 3.14159
+
 #endif
 _HERE_
 
-${_MKCONFIG_SHELL} ${script} -d `pwd` -C ${_MKCONFIG_RUNTESTDIR}/d-cdefint.dat
+${_MKCONFIG_SHELL} ${script} -d `pwd` -C ${_MKCONFIG_RUNTESTDIR}/d-cdefine.dat
 grc=0
 
-egrep -l "^enum (: )?bool ({ )?_cdefint_a = true( })?;$" dcdefint.d > /dev/null 2>&1
+egrep -l "^enum (: )?bool ({ )?_cdefine_a = true( })?;$" dcdefine.d > /dev/null 2>&1
 rc=$?
-if [ $rc -ne 0 ]; then grc=1; fi
+if [ $rc -ne 0 ]; then
+  grc=1
+  echo "## failed: a"
+fi
 
-egrep -l "^enum (: )?int ({ )?a = 1( })?;$" dcdefint.d > /dev/null 2>&1
+egrep -l "^enum (: )?int ({ )?a = 1( })?;$" dcdefine.d > /dev/null 2>&1
 rc=$?
-if [ $rc -ne 0 ]; then grc=1; fi
+if [ $rc -ne 0 ]; then
+  grc=1
+  echo "## failed: a enum"
+fi
 
-egrep -l "^enum (: )?bool ({ )?_cdefint_b = true( })?;$" dcdefint.d > /dev/null 2>&1
+egrep -l "^enum (: )?bool ({ )?_cdefine_b = true( })?;$" dcdefine.d > /dev/null 2>&1
 rc=$?
-if [ $rc -ne 0 ]; then grc=1; fi
+if [ $rc -ne 0 ]; then
+  grc=1
+  echo "## failed: b"
+fi
 
-egrep -l "^enum (: )?int ({ )?b = 2( })?;$" dcdefint.d > /dev/null 2>&1
+egrep -l "^enum (: )?int ({ )?b = 2( })?;$" dcdefine.d > /dev/null 2>&1
 rc=$?
-if [ $rc -ne 0 ]; then grc=1; fi
+if [ $rc -ne 0 ]; then
+  grc=1
+  echo "## failed: b enum"
+fi
 
-egrep -l "^enum (: )?bool ({ )?_cdefint_c = false( })?;$" dcdefint.d > /dev/null 2>&1
+egrep -l "^enum (: )?bool ({ )?_cdefine_c = false( })?;$" dcdefine.d > /dev/null 2>&1
 rc=$?
-if [ $rc -ne 0 ]; then grc=1; fi
+if [ $rc -ne 0 ]; then
+  grc=1
+  echo "## failed: c"
+fi
+
+egrep -l "^enum (: )?bool ({ )?_cdefine_d = true( })?;$" dcdefine.d > /dev/null 2>&1
+rc=$?
+if [ $rc -ne 0 ]; then
+  grc=1
+  echo "## failed: d"
+fi
+
+if [ "$DVERSION" = 1 ]; then
+  egrep -l "^string d = \"d\"( })?;$" dcdefine.d > /dev/null 2>&1
+  rc=$?
+else
+  egrep -l "^enum string d = \"d\";$" dcdefine.d > /dev/null 2>&1
+  rc=$?
+fi
+if [ $rc -ne 0 ]; then
+  grc=1
+  echo "## failed: d enum"
+fi
+
+egrep -l "^enum (: )?bool ({ )?_cdefine_e = true( })?;$" dcdefine.d > /dev/null 2>&1
+rc=$?
+if [ $rc -ne 0 ]; then
+  grc=1
+  echo "## failed: e"
+fi
+
+if [ "$DVERSION" = 1 ]; then
+  egrep -l "^string e = \"abc\"( })?;$" dcdefine.d > /dev/null 2>&1
+  rc=$?
+else
+  egrep -l "^enum string e = \"abc\";$" dcdefine.d > /dev/null 2>&1
+  rc=$?
+fi
+if [ $rc -ne 0 ]; then
+  grc=1
+  echo "## failed: e enum"
+fi
+
+egrep -l "^enum (: )?bool ({ )?_cdefine_g = true( })?;$" dcdefine.d > /dev/null 2>&1
+rc=$?
+if [ $rc -ne 0 ]; then
+  grc=1
+  echo "## failed: g"
+fi
+
+egrep -l "^enum (: )?int ({ )?g = 0xff( })?;$" dcdefine.d > /dev/null 2>&1
+rc=$?
+if [ $rc -ne 0 ]; then
+  grc=1
+  echo "## failed: g enum"
+fi
+
+egrep -l "^enum (: )?bool ({ )?_cdefine_h = true( })?;$" dcdefine.d > /dev/null 2>&1
+rc=$?
+if [ $rc -ne 0 ]; then
+  grc=1
+  echo "## failed: h"
+fi
+
+egrep -l "^enum (: )?int ({ )?h = 0x400( })?;$" dcdefine.d > /dev/null 2>&1
+rc=$?
+if [ $rc -ne 0 ]; then
+  grc=1
+  echo "## failed: h enum"
+fi
+
+egrep -l "^enum (: )?bool ({ )?_cdefine_pi = true( })?;$" dcdefine.d > /dev/null 2>&1
+rc=$?
+if [ $rc -ne 0 ]; then
+  grc=1
+  echo "## failed: pi"
+fi
+
+egrep -l "^enum (: )?double ({ )?pi = 3.14159( })?;$" dcdefine.d > /dev/null 2>&1
+rc=$?
+if [ $rc -ne 0 ]; then
+  grc=1
+  echo "## failed: pi enum"
+fi
 
 if [ $grc -eq 0 ]; then
-  ${DC} -c ${DFLAGS} dcdefint.d
+  ${DC} -c ${DFLAGS} dcdefine.d
   if [ $? -ne 0 ]; then
-    echo "## compile dcdefint.d failed"
+    echo "## compile dcdefine.d failed"
     grc=1
   fi
 fi
 
 if [ "$stag" != "" ]; then
-  mv dcdefint.d dcdefint.d${stag}
+  mv dcdefine.d dcdefine.d${stag}
   mv mkconfig.log mkconfig.log${stag}
   mv mkconfig.cache mkconfig.cache${stag}
   mv mkconfig_d.vars mkconfig_d.vars${stag}
