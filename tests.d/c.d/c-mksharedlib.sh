5210d9ba9defd3edebea91db081402787cd26cc9
bll <none@none>
2011-09-22 17:21:04 -0700

    all shells -d for ksh93 expansion for sco

--- tests.d/c.d/c-mksharedlib.sh
+++ tests.d/c.d/c-mksharedlib.sh
@@ -3,13 +3,13 @@
 . $_MKCONFIG_DIR/testfuncs.sh
 
 maindodisplay $1 'create shared library'
-maindoquery $1 $_MKC_ONCE
+maindoquery $1 $_MKC_SH
 
 chkccompiler
 getsname $0
 dosetup $@
 
-${_MKCONFIG_SHELL} ${_MKCONFIG_DIR}/mkconfig.sh -d `pwd` -C $_MKCONFIG_RUNTESTDIR/c.env.dat
+${_MKCONFIG_SHELL} ${_MKCONFIG_DIR}/mkconfig.sh -d `pwd` -C $_MKCONFIG_RUNTESTDIR/c-shared.env.dat
 . ./c.env
 
 if [ "${_MKCONFIG_SYSTYPE}" = "BSD" ]; then
@@ -73,7 +73,9 @@ main () { int i, j; i = mkct5(); j = 1; if (i == 10) { j = 0; } return j; }
 ${CC} ${CPPFLAGS} ${CFLAGS} ${SHCFLAGS} -c mkct${i}.c
 
 grc=0
-${_MKCONFIG_SHELL} ${_MKCONFIG_DIR}/mksharedlib.sh -e mkct mkct[51234]${OBJ_EXT}
+set +f
+${_MKCONFIG_SHELL} ${_MKCONFIG_DIR}/mksharedlib.sh -d `pwd` -e mkct mkct[51234]${OBJ_EXT}
+set -f
 rc=$?
 if [ $rc -ne 0 ]; then grc=$rc; fi
 
@@ -81,12 +83,18 @@ shrunpath=""
 if [ "${SHRUNPATH}" != "" ]; then
   shrunpath="${SHRUNPATH}."
 fi
-${CC} ${LDFLAGS} ${SHEXECLINK} -o mkct6a${EXE_EXT} mkct6${OBJ_EXT} ${shrunpath} -L. -lmkct
+${CC} ${LDFLAGS} ${SHEXECLINK} -o mkct6a${EXE_EXT} mkct6${OBJ_EXT} \
+    ${shrunpath} -L. -lmkct
 rc=$?
 if [ $rc -ne 0 ]; then grc=$rc; fi
 
-./mkct6a
+./mkct6a${EXE_EXT}
 rc=$?
 if [ $rc -ne 0 ]; then grc=$rc; fi
+
+testcleanup mkct6a${EXE_EXT} \
+    mkct1${OBJ_EXT} mkct2${OBJ_EXT} mkct3${OBJ_EXT} \
+    mkct4${OBJ_EXT} mkct5${OBJ_EXT} mkct6${OBJ_EXT} \
+    mkct1.c mkct2.c mkct3.c mkct4.c mkct5.c mkct6.c
 
 exit $grc
