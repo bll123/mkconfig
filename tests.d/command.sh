2199194fa8e022a9f20d360bcf8cacf1cb061095
bll <none@none>
2011-11-07 21:05:27 -0800

    chg loc more tests

--- tests.d/command.sh
+++ tests.d/command.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-. $_MKCONFIG_DIR/testfuncs.sh
+. $_MKCONFIG_DIR/bin/testfuncs.sh
 
 maindodisplay $1 command
 maindoquery $1 $_MKC_SH_PL
@@ -8,7 +8,15 @@ maindoquery $1 $_MKC_SH_PL
 getsname $0
 dosetup $@
 dorunmkc
+
+chkouth '^#define _command_xblah 0$'
 chkouth '^#define _command_sed 1$'
+chkouth '^#define _cmd_loc_sed "/.*/sed"$'
 chkouth '^#define _command_grep 1$'
+chkouth '^#define _cmd_loc_grep "/.*/grep"$'
+chkouth '^#define _command_awk 1$'
+chkouth '^#define _cmd_loc_awk "/.*/[nmg]?awk"$'
+
 testcleanup
+
 exit $grc
