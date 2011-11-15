4af34015f0613aa122f5e502a0c54bb10a6b1992
bll <none@none>
2011-11-15 11:08:14 -0800

    check DVERSION from environment

--- tests.d/d.d/d-compiler.sh
+++ tests.d/d.d/d-compiler.sh
@@ -13,25 +13,38 @@ ${_MKCONFIG_SHELL} ${_MKCONFIG_DIR}/mkconfig.sh -d `pwd` \
     -C $_MKCONFIG_RUNTESTDIR/d.env.dat
 . ./d.env
 
-> d_compiler.d echo '
+case $DVERSION in
+  1|2|3|4)
+    ;;
+  *)
+    grc=1
+    ;;
+esac
+if [ $grc -ne 0 ]; then
+  echo "## invalid D version"
+fi
+
+if [ $grc -eq 0 ]; then
+  > d_compiler.d echo '
 int main (char[][] args) { return 0; }
 '
 
-${_MKCONFIG_SHELL} ${_MKCONFIG_DIR}/mkc.sh -comp -e -c ${DC} \
-    -o d_compiler.exe d_compiler.d >&9
-rc=$?
-if [ $rc -ne 0 ]; then
-  grc=$rc;
-  echo "## compilation failed"
-fi
-if [ -x d_compiler.exe ]; then
-  ./d_compiler.exe
+  ${_MKCONFIG_SHELL} ${_MKCONFIG_DIR}/mkc.sh -comp -e -c ${DC} \
+      -o d_compiler.exe d_compiler.d >&9
   rc=$?
-  if [ $rc -ne 0 ]; then grc=$rc; fi
-else
-  echo "## unable to locate executable"
-  ls -l >&9
-  grc=1
+  if [ $rc -ne 0 ]; then
+    grc=$rc;
+    echo "## compilation failed"
+  fi
+  if [ -x d_compiler.exe ]; then
+    ./d_compiler.exe
+    rc=$?
+    if [ $rc -ne 0 ]; then grc=$rc; fi
+  else
+    echo "## unable to locate executable"
+    ls -l >&9
+    grc=1
+  fi
 fi
 
 testcleanup
