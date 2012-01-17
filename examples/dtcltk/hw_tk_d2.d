62871b0cc71191787548b7e4720df3dff19768a0
bll <none@none>
2012-01-17 01:06:43 -0800

    Initial revision

---
+++ examples/dtcltk/hw_tk_d2.d
@@ -0,0 +1,32 @@
+import std.stdio;
+import std.string;
+
+import tcl.tcl;
+import tk.tk;
+
+int main(string[] args)
+{
+  Tcl_Interp *tcl_interp;
+  Tk_Window mainWindow;
+
+  tcl_interp = Tcl_CreateInterp();
+  if (Tcl_Init(tcl_interp) != TCL_OK || Tk_Init(tcl_interp) != TCL_OK) {
+    if(*tcl_interp.result) {
+      printf ("A:%s\n", tcl_interp.result);
+    }
+    return 1;
+  }
+
+  mainWindow = Tk_MainWindow(tcl_interp);
+  if (mainWindow == null) {
+    printf ("B:%s\n", tcl_interp.result);
+    return 1;
+  }
+
+  Tcl_Eval (tcl_interp, "label .w -text \"Hello World\"");
+  Tcl_Eval (tcl_interp, "pack .w");
+
+  Tk_MainLoop ();
+
+  return 0;
+}
