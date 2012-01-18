57e92cc9f78f4b048d798fd284a2d78aadcc805b
bll <none@none>
2012-01-18 13:42:00 -0800

    more checks; unknown var tests

--- tests.d/c.d/c-if.sh
+++ tests.d/c.d/c-if.sh
@@ -20,7 +20,7 @@ ${_MKCONFIG_SHELL} ${_MKCONFIG_DIR}/mkconfig.sh -d `pwd` \
 dorunmkc
 for t in \
     _var_a _var_b \
-    _test_b1_ok _test_b2_ok _test_b3_ok _test_b4_ok \
+    _test_b1_ok _test_b2_ok _test_b3_ok _test_b4_ok _test_b5_ok \
     _test_a1_ok _test_a2_ok _test_a3_ok _test_a4_ok _test_a5_ok \
     _test_o1_ok _test_o2_ok _test_o3_ok _test_o4_ok \
         _test_o5_ok _test_o6_ok \
@@ -33,6 +33,21 @@ for t in \
   echo "chk: $t (1)"
   chkouth "^#define ${t} 1$"
 done
+for t in _var_c _var_d _test_b6_ok; do
+  echo "chk: $t (0)"
+  chkouth "^#define ${t} 0$"
+done
+
+t=_var_e
+echo "chk: $t (abc)"
+chkouth "^#define ${t} \"abc\"$"
+t=_var_f
+echo "chk: $t (abc def)"
+chkouth "^#define ${t} \"abc def\"$"
+t=_var_g
+echo "chk: $t (abc def ghi)"
+chkouth "^#define ${t} \"abc def ghi\"$"
+
 chkouthcompile
 
 testcleanup
