90dd904547cb9ccb171d03aee285e1445c0dcdce
bll <none@none>
2012-11-10 17:53:25 -0800

    Initial revision

---
+++ tests.d/grep.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+. $_MKCONFIG_DIR/bin/testfuncs.sh
+
+maindodisplay $1 grep
+maindoquery $1 $_MKC_SH_PL
+
+getsname $0
+dosetup $@
+
+> ${_MKCONFIG_TSTRUNTMPDIR}/gfile echo '
+test1a="blah"
+'
+
+dorunmkc
+
+chkouth '^#define _grep_test1a 1$'
+chkouth '^#define _grep_test1b 0$'
+chkouth '^#define _grep_test1c 1$'
+chkcache "^mkc_c__grep_test1a='1'$"
+chkcache "^mkc_c__grep_test1b='0'$"
+chkcache "^mkc_c__grep_test1c='1'$"
+
+testcleanup
+
+exit $grc
